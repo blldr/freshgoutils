1349568c3b432196966c489fc56e609126327e74
= <=>
2023-03-24 23:22:37 +0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/blldr/freshgoutils
+
+go 1.20
---
+++ stack/stack.go
@@ -0,0 +1,34 @@
+package stack
+
+
+type (
+	Stack[T any] struct {
+		length int
+		head *node[T]
+	}
+	node[T any] struct {
+		value T
+		prev *node[T]
+	}
+)
+
+func NewStack[T any]() *Stack[T] {
+	return &Stack[T] {0, nil}
+}
+
+func (s *Stack[T]) Push(value T){
+	prev := s.head
+	newNode := node[T]{value, prev}
+	s.head = &newNode
+	s.length += 1
+}
+
+func (s *Stack[T]) Pop() *node[T] {
+	if s.length == 0 {
+		return nil
+	}
+	tmp := s.head
+	s.head = tmp.prev
+	s.length -= 1
+	return tmp
+}
